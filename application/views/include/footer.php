1502bef396e582d1c858f4d4244471ab01155ea9
andys <andys@6605fe76-f83e-4952-9af6-e0e39f8b23d7>
2011-03-21 16:03:17 +0000

    updates to header, footer, and sidebar
    git-svn-id: http://devsauce.com/svn/h4xedradio/branches/production-1.1@51 6605fe76-f83e-4952-9af6-e0e39f8b23d7

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,3 +1,23 @@
+<script type='text/javascript'>
+
+var _ues = {
+host:'h4xed.userecho.com',
+forum:'1342',
+lang:'en',
+tab_alignment:'right',
+tab_text_color:'black',
+tab_bg_color:'#FF3300',
+tab_hover_color:'#FF6208'
+};
+
+(function() {
+    var _ue = document.createElement('script'); _ue.type = 'text/javascript'; _ue.async = true;
+    _ue.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
+  })();
+
+</script>
+
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
