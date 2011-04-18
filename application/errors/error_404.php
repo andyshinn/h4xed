c086e8fb2b389625d326757bcaa62c8832a4b278
andys <andys@6605fe76-f83e-4952-9af6-e0e39f8b23d7>
2011-04-18 05:52:02 +0000

    started custom error_404.php page based on main page html
    git-svn-id: http://devsauce.com/svn/h4xedradio/trunk@121 6605fe76-f83e-4952-9af6-e0e39f8b23d7

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -1,34 +1,82 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <title>404 Page Not Found</title>
-<style type="text/css">
-
-body {
-background-color:	#fff;
-margin:				40px;
-font-family:		Lucida Grande, Verdana, Sans-serif;
-font-size:			12px;
-color:				#000;
-}
-
-#content  {
-border:				#999 1px solid;
-background-color:	#fff;
-padding:			20px 20px 12px 20px;
-}
-
-h1 {
-font-weight:		normal;
-font-size:			14px;
-color:				#990000;
-margin:				0 0 4px 0;
-}
-</style>
+<link type="text/css" rel="stylesheet" href="/assets/css/style.css" />
 </head>
 <body>
-	<div id="content">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+<div id="container">
+<div id="main_wrapper">
+<div id="head">
+<div id="logo"><a href="/radio/news"><img
+	src="/assets/images/header-05-f.png" alt="" /></a></div>
+
+<div id="head_links">
+
+<ul>
+
+	<li><a href="/radio/news"><span class="orange">home</span></a></li>
+
+	<li><a href="/radio/tunein">tune<span class="orange">in</span></a></li>
+
+	<li><a href="/playlist">play<span class="orange">list</span></a></li>
+
+	<li><a href="/radio/contact">contact<span class="orange">us</span></a>
+
+	</li>
+
+</ul>
+
+</div>
+</div>
+<div id="main">
+<div id="content">
+<h1><?php
+echo $heading;
+?></h1>
+<p><?php
+echo $message;
+?></p>
+</div>
+</div>
+</div>
+</div>
+
+<script type='text/javascript'>
+
+var _ues = {
+host:'h4xed.userecho.com',
+forum:'1342',
+lang:'en',
+tab_icon_show:false,
+tab_corner_radius:0,
+tab_font_size:24,
+tab_image_hash:'RmVlZGJhY2s%3D',
+tab_alignment:'right',
+tab_text_color:'#FFFFFF',
+tab_bg_color:'#591616',
+tab_hover_color:'#8C2920'
+};
+
+(function() {
+    var _ue = document.createElement('script'); _ue.type = 'text/javascript'; _ue.async = true;
+    _ue.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
+  })();
+
+</script>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-427556-5");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
 </body>
 </html>
