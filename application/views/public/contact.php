413a484e125d6a47bb0779330bd4f6818e8c669a
andys <andys@6605fe76-f83e-4952-9af6-e0e39f8b23d7>
2011-04-18 05:36:23 +0000

    update contacts to add twitter
    git-svn-id: http://devsauce.com/svn/h4xedradio/trunk@119 6605fe76-f83e-4952-9af6-e0e39f8b23d7

--- application/views/public/contact.php
+++ application/views/public/contact.php
@@ -4,5 +4,6 @@
 	<ul>
 		<li>Email: <?php echo mailto('radio@h4xed.us') ?></li>
 		<li>Support Forums: <a href="http://h4xed.userecho.com/" rel="external">http://h4xed.userecho.com/</a></li>
+		<li>Twitter: <a rel="external" href="http://twitter.com/#!/h4xedradio">http://twitter.com/h4xedradio</a></li>
 	</ul>
 </p>
