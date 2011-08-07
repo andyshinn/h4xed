17305ec9898bec06cef9d6193a9f7f721b3bb725
andys <andys@6605fe76-f83e-4952-9af6-e0e39f8b23d7>
2011-08-07 01:11:40 +0000

    fix newlines to use PHP_EOL
    git-svn-id: http://devsauce.com/svn/h4xedradio/trunk@131 6605fe76-f83e-4952-9af6-e0e39f8b23d7

--- application/controllers/playlist.php
+++ application/controllers/playlist.php
@@ -174,9 +174,7 @@ class Playlist extends CI_Controller
                 $remaining_seconds = 0;
                 $current_song = $this->playlist->current_song()->row();
                 $remaining_seconds = ((strtotime($current_song->date_played) + ceil(($current_song->duration / 1000))) - time());
-                echo $current_song->artist . "\n";
-                echo $current_song->title . "\n";
-                echo $remaining_seconds;
+                echo $current_song->artist . PHP_EOL . $current_song->title . PHP_EOL . $remaining_seconds;
             break;
             
             default :
